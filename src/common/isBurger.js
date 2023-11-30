93b9e2c0d31a825b2e08e8f37aef36ec8415b285
rbarannikirum <rbarannikitrum@gmail.com>
2023-11-30 13:51:19 +0300

    fix adaptive from desktop to tablet

--- src/common/isBurger.js
+++ src/common/isBurger.js
@@ -3,30 +3,20 @@ let lastKnownScrollPosition = 0;
 let ticking = false;
 
 document.querySelector(".burger").addEventListener("click", function () {
-  document
-    .getElementById("burger__navigation__tablet")
-    .classList.toggle("burger__navigation__active");
+  document.getElementById("burger__navigation__tablet").classList.toggle("burger__navigation__active");
   document.getElementById("burger").classList.add("burger__scroll");
 
   this.classList.toggle("active");
   document.querySelector(".nav").classList.toggle("open");
 
   if ([...this.classList].includes("active")) {
-    document
-      .getElementById("desktop_image")
-      .classList.add("about__container__call__tablet__decoration");
+    document.getElementById("desktop_image").classList.add("about__container__call__tablet__decoration");
 
-    document
-      .getElementById("header__image")
-      .classList.add("header__navigation__logo__tablet__onScroll");
+    document.getElementById("header__image").classList.add("header__navigation__logo__tablet__onScroll");
     //auth font weight
-    document
-      .querySelector(".header__contact")
-      .classList.toggle("header__contact__light");
-    document
-      .getElementById("header")
-      .classList.add("header__background__scroll");
-    if (window.screen.width <= 768) {
+    document.querySelector(".header__contact").classList.toggle("header__contact__light");
+    document.getElementById("header").classList.add("header__background__scroll");
+    if (window.screen.width <= 980) {
       document.getElementById("header").classList.remove("header__background");
     }
     return;
@@ -34,37 +24,21 @@ document.querySelector(".burger").addEventListener("click", function () {
 
   document.getElementById("desktop_image").removeAttribute("class");
   if (!ticking && window.screen.width <= 320) {
-    document
-      .getElementById("desktop_image")
-      .setAttribute("class", "header__navigation__logo__desktop ");
+    document.getElementById("desktop_image").setAttribute("class", "header__navigation__logo__desktop ");
     document.getElementById("header").classList.remove("header__background");
   }
   document.getElementById("header__image").removeAttribute("class");
-  document
-    .getElementById("header__image")
-    .setAttribute("class", "header__navigation__logo__tablet");
-  document
-    .getElementById("header__image")
-    .classList.add("header__navigation__logo__desktop");
-  document
-    .getElementById("header")
-    .classList.replace("header__background__scroll", "header__background");
+  document.getElementById("header__image").setAttribute("class", "header__navigation__logo__tablet");
+  document.getElementById("header__image").classList.add("header__navigation__logo__desktop");
+  document.getElementById("header").classList.replace("header__background__scroll", "header__background");
   //auth font weight
-  document
-    .querySelector(".header__contact")
-    .classList.toggle("header__contact__light");
+  document.querySelector(".header__contact").classList.toggle("header__contact__light");
   if (lastKnownScrollPosition) {
     //replace image source
-    document
-      .getElementById("desktop_image")
-      .classList.add("about__container__call__tablet__decoration");
-    document
-      .getElementById("header__image")
-      .classList.add("header__navigation__logo__tablet__onScroll");
+    document.getElementById("desktop_image").classList.add("about__container__call__tablet__decoration");
+    document.getElementById("header__image").classList.add("header__navigation__logo__tablet__onScroll");
     // change bg color
-    document
-      .getElementById("header")
-      .classList.replace("header__background", "header__background__scroll");
+    document.getElementById("header").classList.replace("header__background", "header__background__scroll");
     return;
   }
 });
@@ -76,42 +50,35 @@ function doSomething(scrollPos) {
   // Do something with the scroll position
 
   const header_position_fixed = document.getElementById("header");
+  const promo = document.getElementById("promo");
   const header_image = document.getElementById("header__image");
   const desktop_image = document.getElementById("desktop_image");
   const burger = document.getElementById("burger");
+  // const navigationText = document.querySelectorAll("nav")
 
+  // promo.classList.toggle("promo__container__fixed")
   if (scrollPos) {
-    header_position_fixed.setAttribute(
-      "class",
-      "header header__background__scroll"
-    );
+    header_position_fixed.setAttribute("class", "header header__background__scroll about__header");
+    if (window.screen.width <= 980) {
+      header_position_fixed.setAttribute("class", "header header__background__scroll");
+    }
+    promo.classList.add("promo__container__fixed");
     //burger before and after elements color
     burger.classList.add("burger__scroll");
-    header_image.setAttribute(
-      "class",
-      "header__navigation__logo__tablet__onScroll"
-    );
-
-    desktop_image.setAttribute(
-      "class",
-      "about__container__call__tablet__decoration"
-    );
+    header_image.setAttribute("class", "header__navigation__logo__tablet__onScroll");
+    // navigationText.classList.add("nav__scroll")
+    desktop_image.setAttribute("class", "about__container__call__tablet__decoration");
 
     return;
   }
-
+  // promo.classList.toggle("promo__container__fixed")
+  promo.classList.remove("promo__container__fixed");
   header_image.setAttribute("class", "header__navigation__logo__tablet");
   desktop_image.setAttribute("class", "header__navigation__logo__desktop");
 
   if ([...burger.classList].includes("active")) {
-    header_image.setAttribute(
-      "class",
-      "header__navigation__logo__tablet__onScroll"
-    );
-    desktop_image.setAttribute(
-      "class",
-      "about__container__call__tablet__decoration"
-    );
+    header_image.setAttribute("class", "header__navigation__logo__tablet__onScroll");
+    desktop_image.setAttribute("class", "about__container__call__tablet__decoration");
     return;
   }
 
@@ -122,22 +89,15 @@ function doSomething(scrollPos) {
 document.addEventListener("scroll", (event) => {
   lastKnownScrollPosition = window.scrollY;
 
-  if (!ticking && window.screen.width <= 780) {
-    window.requestAnimationFrame(() => {
-      doSomething(lastKnownScrollPosition);
-      ticking = false;
-    });
-
-    ticking = true;
-  }
+  window.requestAnimationFrame(() => {
+    doSomething(lastKnownScrollPosition);
+  });
 });
 
 function burgerScrollTo(value) {
-  document
-    .getElementById("burger__navigation__tablet")
-    .classList.toggle("burger__navigation__active");
+  document.getElementById("burger__navigation__tablet").classList.toggle("burger__navigation__active");
   document.querySelector(".burger").classList.toggle("active");
   document.querySelector(".nav").classList.toggle("open");
-  
+
   scrollTo1(value);
 }
--- src/common/scrollTo.js
+++ src/common/scrollTo.js
@@ -1,23 +1,4 @@
 // navigation scroll (menu bar)
 function scrollTo1(value) {
-  console.log("clicked to", value);
-  if (!ticking && window.screen.width <= 320) {
-    switch (value + "") {
-      case "1060":
-        window.scrollTo(0, 970);
-        break;
-      case "1820":
-        window.scrollTo(0, 1350);
-        break;
-      case "2900":
-        window.scrollTo(0, 2330);
-        break;
-      default:
-        console.log("some undefined value");
-        break;
-    }
-    return;
-  }
-
-  window.scrollTo(0, value);
+  document.getElementById(value).scrollIntoView({behavior: "smooth", block: "center"})
 }
